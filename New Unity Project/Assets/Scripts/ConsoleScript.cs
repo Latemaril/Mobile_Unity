62c23c74b809c4383398d3b32171c5b4bb0c0fcf
Sukhovey Vladislav <sukhovey.vlad@gmail.com>
2024-02-06 11:42:54 +1000

    first commit

---
+++ New Unity Project/Assets/Scripts/ConsoleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleScript : MonoBehaviour
+{
+    public int age = 500;
+ 
+    void Start()
+    {
+        Debug.Log("Дарова попущи! Мне " + age + " лет");
+    }
+
+
+    void Update()
+    {
+        float sinus = Mathf.Sin(Time.time);
+        float d = Mathf.Abs(sinus);
+        float t = Mathf.Lerp(1, 2, d);
+
+        transform.Rotate(0, 0.5f, 0);
+        transform.localScale = new Vector3(t, t, t);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/TransformTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformTest : MonoBehaviour
+{
+    GameObject[] transfromCubes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transfromCubes = GameObject.FindGameObjectsWithTag("TransformTest");
+
+        for (int i = 0; i < transfromCubes.Length; i++)
+        {
+            transfromCubes[i].transform.position = new Vector3((-40 + (i*2)), 0.5f, 0);
+            Debug.Log("transfromCubes[i].transform.position  " + transfromCubes[i].transform.position);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
